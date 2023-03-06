652529a979b411537420242b8bd945d65abd1a11
Luis Angulo <luisangulo97@hotmail.com>
2023-03-06 18:29:24 -0500

    Quinto Commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { MdVideoLibrary } from "react-icons/md";
+import { MdVideoLibrary, MdPlayCircleFilled } from "react-icons/md";
 
 export const Header = () => {
     return (
@@ -8,15 +8,23 @@ export const Header = () => {
                 <div className='mt-20 mb-5 w-full text-right font-bold text-4xl'>
                     Star Wars: Clone Wars
                 </div>
+                <div className='flex gap-2 mb-2'>
+                    <div className='rounded-full border-blue-500 border-2 bg-blue-500 px-2'>Adventure</div>
+                    <div className='rounded-full border-blue-500 border-2 bg-blue-500 px-2'>Fantasy</div>
+                    <div className='rounded-full border-blue-500 border-2 bg-blue-500 px-2'>Action</div>
+                </div>
+                <div className='my-1.5 text-slate-100'>
+                   Español - Diciembre 10 1997(USA)- 2h 36m 
+                </div>
                 <div className='mb-8 text-right w-full font-semibold text-gray-300'>
                     Star Wars, traducido al español como La Guerra de las Galaxias, es una franquicia compuesta de películas, novelas, cómics, videojuegos y juguetes.
                 </div>
                 <div className=' w-full flex gap-5 justify-end items-center'>
                     <div>
-                        <button className='rounded-full border-blue-900 border-8 bg-blue-900'>Wacth Trailer</button >
+                        <button className='rounded-full flex items-center gap-2 p-1 border-blue-900 border-8 bg-blue-900'>Wacth Trailer<MdVideoLibrary/></button >
                     </div>
                     <div>
-                        <button className='rounded-full border-green-500 border-8 bg-green-500'>Wacth Movie</button>
+                        <button className='rounded-full flex items-center gap-2 p-1 border-green-500 border-8 bg-green-500'>Wacth Movie<MdPlayCircleFilled/></button>
                     </div>
                 </div>
             </div>
