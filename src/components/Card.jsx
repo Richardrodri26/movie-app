4b43a2c6e3da34c78a42fef15f36a4bd3aa0d526
Luis Angulo <luisangulo97@hotmail.com>
2023-03-06 20:37:45 -0500

    Sexto commit

---
+++ src/components/Card.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+export const Card = ({imgUrl, keyId}) => {
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+  const url = URL_IMAGE + imgUrl
+    
+    return (
+        <div className='h-80 w-auto' key={keyId}>
+            <img className='w-44 h-2/3 rounded-t-lg bg-cover'
+                src={url}
+            />
+            <div className='w-auto h-1/3 bg-blue-200 rounded-b-lg'>
+                <p className='text-white text-bold text-xl px-4 py-2'>Norway</p>
+                <p className='text-white text-md px-4 py-2'>Beautiful Landscape</p>
+            </div>
+        </div>
+    )
+}
+
--- src/pages/MoviesPage.jsx
+++ src/pages/MoviesPage.jsx
@@ -1,9 +1,56 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import { Card } from '../components/Card'
+import axios from "axios";
 
 export const MoviesPage = () => {
-  return (
-    <div className='bg-yellow-900 w-screen h-screen'>
+  const API_KEY = "a6e8bd7e88f8039d24bcd9c29cd51230";
 
+  const API_URL = "https://api.themoviedb.org/3";
+  const IMAGE_PATH = "https://image.tmdb.org/t/p/original";
+
+  // endpoint para las imagenes
+  const URL_IMAGE = "https://image.tmdb.org/t/p/original";
+
+  const [movies, setMovies] = useState([]);
+  const [searchKey, setSearchKey] = useState("");
+  //const [selectedMovie, setSelectedMovie] = useState({})
+  const [movie, setMovie] = useState({ title: "Loading Movies" });
+  //const [playing, setPlaying] = useState(false);
+
+  // funcion para realizar la peticion get a la api
+  const fetchMovies = async (searchKey) => {
+    const type = searchKey ? "search" : "discover";
+    const {
+      data: { results },
+    } = await axios.get(`${API_URL}/${type}/movie`, {
+      params: {
+        api_key: API_KEY,
+        query: searchKey,
+      },
+    });
+    //console.log("data", results);
+    //setSelectedMovie(results[0])
+
+    setMovies(results);
+    setMovie(results[0]);
+    //console.log(movies[0].title);
+  };
+
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+
+  return (
+    <div className='bg-red-500 h-auto w-auto'>
+      <div className='scrollbar-hide w-auto h-auto p-2 items-center mt-6 overflow-x-auto overscroll-x-contain flex space-x-6 overflow-y-hidden'>
+
+        {
+          movies.map((movie) => (
+            <Card imgUrl={movie.poster_path} keyId={movie.id}/>
+          ))
+        }
+        
+      </div>
     </div>
   )
 }
