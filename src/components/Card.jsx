2328a7b3c982928d3f02de34ace58758e73714b1
Richardrodri26 <richardmanuel811@gmail.com>
2023-03-09 20:58:37 -0500

    refactor of card component

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,34 +2,31 @@ import React, { useContext, useEffect } from "react";
 import { Link } from "react-router-dom";
 import MoviesContext from "../context/movies/MoviesContext";
 
-export const Card = () => {
-  const { movies, getMovies } = useContext(MoviesContext);
+export const Card = (props) => {
   const URL_IMAGE = "https://image.tmdb.org/t/p/original";
 
   return (
     <div className="flex justify-center gap-5">
-      {movies.map((movie) => (
         <div
           className="block min-w-[200px] rounded-lg bg-slate-500"
-          key={movie.id}
+          key={props.id}
         >
-          <Link to={`/movies/${movie.id}`}>
+          <Link to={`/movies/${props.id}`}>
             <img
               className="rounded-t-lg"
-              src={`${URL_IMAGE}${movie.poster_path}`}
+              src={`${URL_IMAGE}${props.posterImg}`}
               alt=""
             />
           </Link>
           <div className="p-2  ">
             <h5 className="mb-2 min-h-[116px] text-xl font-medium leading-tight text-neutral-800">
-              {movie.title}
+              {props.title}
             </h5>
             <p className="mb-4 text-base text-neutral-600">
-              {movie.popularity}
+              {props.popularity}
             </p>
           </div>
         </div>
-      ))}
     </div>
   );
 };
--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -9,9 +9,11 @@ export const Carousel = () => {
       <div className="scrollbar-hide mt-10 w-auto h-auto p-8 items-center overflow-x-auto overscroll-x-auto flex space-x-6 overflow-y-auto">
         {movies.map((movie) => (
           <Card
+            id={movie.id}
             imgUrl={movie.poster_path}
             title={movie.original_title}
             popularity={movie.popularity}
+            posterImg={movie.poster_path}
             key={movie.id}
           />
         ))}
