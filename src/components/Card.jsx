7f148c52a5ebf5b21b9fdc6ad2896d6cbd2c775b
Luis Angulo <luisangulo97@hotmail.com>
2023-03-14 16:41:35 -0500

    Card updated

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -7,7 +7,7 @@ export const Card = (props) => {
     <div className="flex justify-center gap-5">
       <Link to={`/movies/${props.id}`}>
         <div
-          className="block min-w-[200px] relative z-0 group hover:shadow-lg hover:shadow-red-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-150"
+          className="block min-w-[200px] hover:mx-5 relative z-0 group hover:shadow-lg hover:shadow-red-500 transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 hover:duration-150"
           key={props.id}
         >
 
@@ -17,20 +17,22 @@ export const Card = (props) => {
             alt=""
           />
 
-          <div className="flex flex-col justify-between rounded-md w-full h-full bg-neutral-700 p-4 absolute -bottom-1 left-0 z-20 opacity-0 group-hover:opacity-100 transition ease-in-out delay-150 group-hover:-translate-y-1  group-hover:duration-150">
-            <img 
+          <div className="flex flex-col justify-between rounded-md w-full h-full bg-neutral-700 p-4 absolute -bottom-1 left-0 z-20 opacity-0 group-hover:opacity-100 transition ease-in-out delay-150 group-hover:-translate-y-1  group-hover:duration-150 hover:scale-110">
+            <img
               className="w-full h-auto absolute top-0 left-0"
               src={`${URL_IMAGE}${props.banner}`}
-              alt={props.title}/>
-            <h5 className="relative mb-2 h-auto text-xl font-medium leading-tight text-white">
-              {props.title}
-            </h5>
-            <p className="relative mb-4 text-base text-gray-300 font-sans">
-              {props.description.substring(0, 100) + '...'}
-            </p>
-            <p className="relative mb-4 text-base text-red-500 font-medium">
-              {`${props.popularity} 🔥`}
-            </p>
+              alt={props.title} />
+            <div className="relative top-[110px] h-[60%] flex flex-col justify-between">
+              <h5 className="text-lg font-medium leading-tight text-white">
+                {props.title}
+              </h5>
+              <p className="text-sm text-gray-300 font-sans">
+                {props.description.substring(0, 65) + '...'}
+              </p>
+              <p className="text-base text-red-500 font-medium">
+                {`${props.popularity} 🔥`}
+              </p>
+            </div>
           </div>
         </div>
       </Link>
